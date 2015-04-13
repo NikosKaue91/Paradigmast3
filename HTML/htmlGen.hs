cac4b0d04967f948b7e0511b2960e9fbc51c2ba5
NikosKaue <ngomes@inf.ufsm.br>
2015-04-13 01:59:06 -0300

    finalizado
    tarefa do html

--- HTML/htmlGen.hs
+++ HTML/htmlGen.hs
@@ -1,24 +1,36 @@
---
--- Este programa lÃª um arquivo CSV, com 2 dados por linha. Cada linha Ã© transformada numa tupla. 
--- A lista de tuplas Ã© passada para uma funÃ§Ã£o que vai gerar uma longa string representando um
--- documento HTML.
---
+--Gerando HTML: Programação funcional com Haskell
+--Nome: Nikollas Kauê Soares Gomes
+--Matrícula: 201020867
+
+{- Este programa lê um arquivo CSV, com 2 dados por linha. Cada linha é transformada numa tupla. 
+  A lista de tuplas são passada para uma função que vai gerar uma longa string representando um
+  documento HTML. -}
 
 main :: IO ()
 main = do
     strcontent <- readFile infile
     let listofstrlist = map (splitOnChar ',') (lines strcontent)
         strtuplelist = map (\lis -> (head lis, last lis)) listofstrlist
-    writeFile outfile (mkHtmlURLItemsDoc "UsuÃ¡rios Cadastrados no NCC" strtuplelist)
+    writeFile outfile (mkHtmlURLItemsDoc "Usuarios Cadastrados no NCC:" strtuplelist)
     where 
     infile = "logins.csv"
-    outfile = "output.html"
+    outfile = "cadastros-NCC.html"
 
 
--- Esta funÃ§Ã£o deve ser alterada para chamar outras funÃ§Ãµes que vÃ£o
--- construir o documento HTML
+{- Esta função deve ser alterada para chamar outras funções que vão
+  construir o documento HTML. 
+  <a href="nome-do-lugar-a-ser-levado">descrição</a> -}
 mkHtmlURLItemsDoc :: String -> [(String,String)] -> String
-mkHtmlURLItemsDoc title lis = "UsuÃ¡rios Cadastrados no NCC\n" ++ unlines (map fst lis)
+mkHtmlURLItemsDoc title lis = title ++ "<html><body><ul>" ++ (foldr1 (++) (end (link (map fst lis) (map snd lis)))) ++ "<html><body><ul>"
+
+--começo da implementação
+link :: [String] -> [String] -> [String]
+link x y
+    | (x == []) || (y == []) = []
+    | otherwise = ("<li><a href=http://www.inf.ufsm.br/~"++ (head y)++ ">" ++ (head x)) : link (tail x) (tail y) --irá montar o link de redirecionamento do usuario cadastrado no arquivo logins
+
+end :: [String] -> [String]
+end s = map (++"</a></li>\n") s --encerramento da implementação 
 
 
 -- Decompoe string usando um caracter delimitador
