be1adcac68b5d9f7e2c9a98370f63353a2962dab
NikosKaue <ngomes@inf.ufsm.br>
2015-04-13 01:00:30 -0300

    login
    Finalizado, realizado os testes finais

--- Login/t3-logins.hs
+++ Login/t3-logins.hs
@@ -1,3 +1,19 @@
 --Trabalho de Lista de Logins: Programação funcional com Haskell
 --Nome: Nikollas Kauê Soares Gomes
 --Matrícula: 201020867
+import Data.Char
+main :: IO ()
+main = do
+    strcontent <- readFile "nomes.csv"
+    let strlist = lines strcontent
+        strnew = [str ++ "," ++ userName str | str <- strlist]
+    writeFile "logins.csv" (unlines strnew)
+	
+userName :: String -> String
+userName name = map (toLower)(takeWhile (/= name !! 1)name ++ (reverse (takeWhile (/=' ')(reverse name))))
+
+lastName :: String -> String
+lastName name 
+	| filter (==' ')  name == [] = name
+	| otherwise = lastName (tail name)
+
